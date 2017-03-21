1a4aa1b957222368fcc19b283a4200499b21d769
Totta Wang <totta.wang@autodesk.com>
2017-03-21 14:33:18 +0800

    add transient getters for verification

--- src/main/java/com/sample/domain/Product.java
+++ src/main/java/com/sample/domain/Product.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import com.datastax.driver.mapping.annotations.ClusteringColumn;
 import com.datastax.driver.mapping.annotations.PartitionKey;
 import com.datastax.driver.mapping.annotations.Table;
+import com.datastax.driver.mapping.annotations.Transient;
 import com.sample.conf.EnableBatch;
 
 @Table(name = "products")
@@ -20,10 +21,13 @@ public class Product {
   @ClusteringColumn
   private UUID productid;
 
+  private String text;
+
   public Product(String itemid, int version, UUID productid) {
     this.itemid = itemid;
     this.version = version;
     this.productid = productid;
+    this.text = "dummy";
   }
 
   public UUID getProductid() {
@@ -49,4 +53,25 @@ public class Product {
   public void setItemid(String _itemid) {
     this.itemid = _itemid;
   }
+
+  @Transient
+  public ProductKey getProductKey() {
+    return new ProductKey(this.itemid, this.version, this.productid);
+  }
+
+  public void setProductKey(ProductKey productKey) {
+    this.productid = productKey.getProductid();
+    this.version = productKey.getVersion();
+    this.itemid = productKey.getItemid();
+  }
+
+  @Transient
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
 }
